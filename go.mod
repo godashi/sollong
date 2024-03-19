7a4ea031dc2269c8ba0f78ebb03c8a5ba9e5cb55
knightxv <962068006@qq.com>
2024-03-20 00:15:29 +0800

    import okx

--- go.mod
+++ go.mod
@@ -3,15 +3,49 @@ module wallet/manager
 go 1.21.1
 
 require (
+	github.com/ethereum/go-ethereum v1.13.14
+	github.com/okx/go-wallet-sdk/coins/ethereum v0.0.3
+	github.com/okx/go-wallet-sdk/crypto v0.0.2
 	github.com/spf13/cobra v1.8.0
-	github.com/tyler-smith/go-bip32 v1.0.0
+	github.com/stretchr/testify v1.9.0
 	github.com/tyler-smith/go-bip39 v1.1.0
 )
 
 require (
+	github.com/DataDog/zstd v1.5.5 // indirect
 	github.com/FactomProject/basen v0.0.0-20150613233007-fe3947df716e // indirect
 	github.com/FactomProject/btcutilecc v0.0.0-20130527213604-d3a63a5752ec // indirect
+	github.com/bits-and-blooms/bitset v1.10.0 // indirect
+	github.com/btcsuite/btcd v0.23.4 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.3 // indirect
+	github.com/cockroachdb/pebble v1.1.0 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/crate-crypto/go-kzg-4844 v0.7.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/ethereum/c-kzg-4844 v0.4.0 // indirect
+	github.com/getsentry/sentry-go v0.27.0 // indirect
+	github.com/holiman/uint256 v1.2.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/okx/go-wallet-sdk/util v0.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/prometheus/client_golang v1.18.0 // indirect
+	github.com/prometheus/client_model v0.6.0 // indirect
+	github.com/prometheus/common v0.47.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	github.com/supranational/blst v0.3.11 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20220721030215-126854af5e6d // indirect
+	github.com/tyler-smith/go-bip32 v1.0.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 )
--- internal/app/wallet_manager/batch_create.go
+++ internal/app/wallet_manager/batch_create.go
@@ -2,71 +2,187 @@ package wallet_manager
 
 import (
 	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"encoding/pem"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
-	"os"
+	"math/big"
 
-	"github.com/tyler-smith/go-bip32"
+	// "github.com/btcsuite/btcd/btcec"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/okx/go-wallet-sdk/coins/ethereum"
+	"github.com/okx/go-wallet-sdk/crypto/bip32"
 	"github.com/tyler-smith/go-bip39"
 )
 
-type CurrencyWallet struct {
+func GenerateMnemonic() (string, error) {
+
+	entropy, err := bip39.NewEntropy(256)
+	if err != nil {
+		return "", err
+	}
+	mnemonic, err := bip39.NewMnemonic(entropy)
+	return mnemonic, err
 }
 
-func CreateCurrencyWallet() *CurrencyWallet {
-	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	privateKeyBytes, err := x509.MarshalECPrivateKey(privateKey)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	privateKeyPem := &pem.Block{
-		Type:  "EC PRIVATE KEY",
-		Bytes: privateKeyBytes,
-	}
-	pemFile, _ := os.Create("privateKey.pem")
-	pem.Encode(pemFile, privateKeyPem)
-	pemFile.Close()
-
-	publicKey := privateKey.PublicKey
-	publicKeyBytes, err := x509.MarshalPKIXPublicKey(&publicKey)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	publicKeyPem := &pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: publicKeyBytes,
-	}
-	pemFile, _ = os.Create("publicKey.pem")
-	pem.Encode(pemFile, publicKeyPem)
-	pemFile.Close()
-
-	fmt.Println("密钥对生成成功！")
-	return nil
+func GetDerivedPath(index int) string {
+	return fmt.Sprintf(`m/44'/60'/0'/0/%d`, index)
 }
 
-func CreateCurrencyWallet2() {
-	// Generate a mnemonic for memorization or user-friendly seeds
-	entropy, _ := bip39.NewEntropy(128)
-	mnemonic, _ := bip39.NewMnemonic(entropy)
+func GetDerivedPrivateKey(mnemonic string, hdPath string) (string, error) {
+	seed := bip39.NewSeed(mnemonic, "")
+	rp, err := bip32.NewMasterKey(seed)
+	if err != nil {
+		return "", err
+	}
+	c, err := rp.NewChildKeyByPathString(hdPath)
+	if err != nil {
+		return "", err
+	}
+	childPrivateKey := hex.EncodeToString(c.Key.Key)
+	return childPrivateKey, nil
+}
 
-	// Generate a Bip32 HD wallet for the mnemonic and a user supplied password
-	seed := bip39.NewSeed("scissors innocent bulk present time affair parade fuel thought scrub marriage ice cause laundry stereo spider home dish know repeat flip rude bonus seed", "Secret Passphrase")
+// func GetNewAddress(prvHex string) string {
+// 	prvBytes, err := hex.DecodeString(prvHex)
+// 	if err != nil {
+// 		return ""
+// 	}
+// 	prv, pub := btcec.PrivKeyFromBytes(btcec.S256(), prvBytes)
+// 	if prv == nil {
+// 		return ""
+// 	}
+// 	return ethereum.GetAddress(hex.EncodeToString(pub.SerializeCompressed()))
+// }
 
-	masterKey, _ := bip32.NewMasterKey(seed)
-	publicKey := masterKey.PublicKey()
+func ValidAddress(address string) bool {
+	return ethereum.ValidateAddress(address)
+}
 
-	// Display mnemonic and keys
-	fmt.Println("Mnemonic: ", mnemonic)
-	fmt.Println("Master private key: ", masterKey)
-	fmt.Println("Master public key: ", publicKey)
+type SignParams struct {
+	Type                 int    `json:"type"`
+	ChainId              string `json:"chainId"`
+	Nonce                string `json:"nonce"`
+	MaxPriorityFeePerGas string `json:"maxPriorityFeePerGas"`
+	MaxFeePerGas         string `json:"maxFeePerGas"`
+	GasLimit             string `json:"gasLimit"`
+	To                   string `json:"to"`
+	Value                string `json:"value"`
+	Data                 string `json:"data"`
+	isToken              bool   `json:"isToken"`
+}
 
-	//  Mnemonic:  scissors innocent bulk present time affair parade fuel thought scrub marriage ice cause laundry stereo spider home dish know repeat flip rude bonus seed
-	//  Master private key:  xprv9s21ZrQH143K3VpQJFVKLeNDV3XjphmPa6UmBHr89ESwZijKoj1dfQy3mNSn4DiRggrgJJJEeE6YjZZzZC7AvKNmztmy9isshgpcy9taDR7
-	//  Master public key:  xpub661MyMwAqRbcFytsQH2KhnJx35NEEAVEwKQMygFjhZyvSX4UMGKtDDHXcf5pnaq5YoJJtiYBWBeRfaYsKu9m7Ajgo1D4BzcE3oFQpcTymU8
+func SignEip1559Transaction(chainId *big.Int, tx *types.Transaction, prvKey *ecdsa.PrivateKey) ([]byte, string, error) {
+	signer := types.NewLondonSigner(chainId)
+	signedTx, err := types.SignTx(tx, signer, prvKey)
+	if err != nil {
+		return nil, "", err
+	}
+	rawTx, err := signedTx.MarshalBinary()
+	if err != nil {
+		return nil, "", err
+	}
+	return rawTx, signedTx.Hash().Hex(), nil
+}
 
+func toJosn(r interface{}) string {
+	res, err := json.Marshal(r)
+	if err != nil {
+		return ""
+	}
+	return string(res)
+}
+
+type SignedTx struct {
+	Hash string `json:"hash"`
+	Hex  string `json:"hex"`
+}
+
+// func SignTransaction(txJson, prvHex string) (string, error) {
+// 	if len(txJson) == 0 {
+// 		return "", errors.New("invalid txJson")
+// 	}
+// 	if len(prvHex) == 0 {
+// 		return "", errors.New("invalid prvHex")
+// 	}
+// 	var err error
+// 	var s SignParams
+// 	if err := json.Unmarshal([]byte(txJson), &s); err != nil {
+// 		return "", err
+// 	}
+// 	chainId := util.ConvertToBigInt(s.ChainId)
+// 	var to *common.Address
+// 	if len(s.To) != 0 {
+// 		addr := common.HexToAddress(s.To)
+// 		to = &addr
+// 	}
+// 	var data []byte
+// 	if len(s.Data) != 0 {
+// 		if data, err = util.DecodeHexString(s.Data); err != nil {
+// 			return "", err
+// 		}
+// 	}
+// 	prvBytes, err := hex.DecodeString(prvHex)
+// 	if err != nil {
+// 		return "", errors.New("invalid prvHex")
+// 	}
+// 	var jsonTx ethereum.Eip1559Token
+// 	if err := json.Unmarshal([]byte(txJson), &jsonTx); err != nil {
+// 		return "", err
+// 	}
+// 	if jsonTx.TxType == types.DynamicFeeTxType { // EIP1559 sign
+// 		prv, _ := btcec.PrivKeyFromBytes(btcec.S256(), prvBytes)
+// 		tx := ethereum.NewEip1559Transaction(
+// 			chainId,
+// 			util.ConvertToBigInt(jsonTx.Nonce).Uint64(),
+// 			util.ConvertToBigInt(jsonTx.MaxPriorityFeePerGas),
+// 			util.ConvertToBigInt(jsonTx.MaxFeePerGas),
+// 			util.ConvertToBigInt(jsonTx.GasLimit).Uint64(),
+// 			to,
+// 			util.ConvertToBigInt(jsonTx.Value),
+// 			data,
+// 		)
+// 		res, hash, err := SignEip1559Transaction(chainId, tx, (*ecdsa.PrivateKey)(prv))
+// 		if err != nil {
+// 			return "", err
+// 		}
+// 		return toJosn(SignedTx{Hash: hash, Hex: util.EncodeHexWith0x(res)}), nil
+// 	} else {
+// 		prv, _ := btcec2.PrivKeyFromBytes(prvBytes)
+// 		// Token processing
+// 		var tx *ethereum.EthTransaction
+// 		if s.isToken {
+// 			tx = ethereum.NewEthTransaction(util.ConvertToBigInt(jsonTx.Nonce), util.ConvertToBigInt(jsonTx.GasLimit), util.ConvertToBigInt(jsonTx.GasPrice), big.NewInt(0), jsonTx.ContractAddress, util.EncodeHexWith0x(data))
+// 		} else {
+// 			tx = ethereum.NewEthTransaction(util.ConvertToBigInt(jsonTx.Nonce), util.ConvertToBigInt(jsonTx.GasLimit), util.ConvertToBigInt(jsonTx.GasPrice), util.ConvertToBigInt(jsonTx.Value), jsonTx.To, util.EncodeHexWith0x(data))
+// 		}
+// 		res, err := tx.SignTransaction(chainId, (*secp256k1.PrivateKey)(prv))
+// 		if err != nil {
+// 			return "", err
+// 		}
+// 		return toJosn(SignedTx{Hash: ethereum.CalTxHash(res), Hex: res}), nil
+// 	}
+// }
+
+func MessageHash(data string) string {
+	return ethereum.MessageHash(data)
+}
+
+func GetEthereumMessagePrefix(message string) string {
+	return ethereum.GetEthereumMessagePrefix(message)
+}
+
+func CalTxHash(rawTx string) string {
+	return ethereum.CalTxHash(rawTx)
+}
+
+func GenerateRawTransactionWithSignature(txType int, chainId, unsignedRawTx, r, s, v string) (string, error) {
+	return ethereum.GenerateRawTransactionWithSignature(txType, chainId, unsignedRawTx, r, s, v)
+}
+func DecodeTx(rawTx string) (string, error) {
+	return ethereum.DecodeTx(rawTx)
+}
+
+func EcRecover(signature, message string, addPrefix bool) string {
+	return ethereum.EcRecover(signature, message, addPrefix)
 }
